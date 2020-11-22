4e9c3527f1acaaa56652b77d116a81955fa00e4a
tara narimani <narimani.tara@yahoo.com>
2020-11-22 04:22:23 +0330

    create url with router to register user

--- myUrlShortener/urls.py
+++ myUrlShortener/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest-framework import routers
+
+from accounts.views import RegisterUserViewSet
+
+router = routers.DefaultRouter()
+router.register('register/', RegisterUserViewSet, basename='register')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls, 'urlShortener')),
 ]
