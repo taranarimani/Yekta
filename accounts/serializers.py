53d5454e9c0d60cf247fd5682c993a74708588a3
tara narimani <narimani.tara@yahoo.com>
2020-11-22 18:23:15 +0330

    write serializer for register user

---
+++ accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest-framework import serializers
+from accounts.models import User
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = [
+            'email', 'username', 'password'
+        ]
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
